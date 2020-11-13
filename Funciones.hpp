c2aa311e7ac8e5d22018dd64bedd1cb7e9d624c8
TitoCarpio <00226620@uca.edu.sv>
2020-11-13 12:34:25 -0600

    actualizacion del menu y comentado

--- Funciones.hpp
+++ Funciones.hpp
@@ -20,6 +20,7 @@ struct Queue {
 };
 typedef struct Queue queue;
 
+//registrode inicio de sesion del usurio.
 struct Sesion{
     string nombre;
     string correo;
@@ -27,8 +28,6 @@ struct Sesion{
 
 };
 typedef struct Sesion registro;
-
-
 
 // Inicializa mi cola.
 void initialize(queue *q) {
@@ -41,7 +40,7 @@ bool empty(queue *q) {
     return (q->front == NULL) ? true : false;
 }
 
-// Ingresa un elemennto a mi cola.
+// Ingresa un elemennto a la cola.
 void push(queue *q, string nombre, string autor, string genero,int tiempo) {
     node *newNode;
     newNode = new node;
@@ -60,7 +59,7 @@ void push(queue *q, string nombre, string autor, string genero,int tiempo) {
     }    
 }
 
-// Elimina un elemento de mi cola.
+// Elimina un elemento de la cola.
 void pop(queue *q) {
     if(empty(q)) {
         cout << "Underflow" << endl;
@@ -76,21 +75,21 @@ void pop(queue *q) {
     delete(aux);
 }
 
-// Funcion que imprime los elementos de mi cola.
+// Funcion que imprime los elementos de la cola.
 void showQueue(queue *q) {
     if(empty(q)) {
-        cout << "La cola esta vacia" << endl;
+        cout << "\t\t\t\t\t\t\tLa cola esta vacia" << endl;
     }else {
         node *aux = q->front;
         while(aux) {
-            cout << "[ "<<aux->titulo << " "<<aux->cantante<<" "<<aux->genero<<" "<<aux->duracion<<" "<<" ]";
-
+            cout << "\t\t\t\t\t\t\t[ "<<aux->titulo << " "<<aux->cantante<<" "<<aux->genero<<" "<<aux->duracion<<" "<<" ]";
+            cout<<endl;
             aux = aux->next;
         }
     }
 }
 
-
+//funcion que elimina un elemento de la cola
 void eliminar(queue *s){
     if(empty(s)){
         
@@ -102,6 +101,7 @@ void eliminar(queue *s){
     
 }
 
+//solicitud de datos del usuario para inicio de sesion
 void inicio (registro *sesion){
     
      cout << endl << endl << endl;
@@ -134,6 +134,7 @@ void inicio (registro *sesion){
     
 }
 
+//solicitud para aceptar terminos y condiciones del reproductor
 void terminosycondiciones( bool * terminos){
     int opcion, status= true;
 
@@ -166,8 +167,304 @@ void terminosycondiciones( bool * terminos){
      
      
     }
+
+}
+
+//menu de usurio.
+void menu(queue *electronica, queue *regueton, queue *rap, queue *Pop,queue *salsa, queue *descargas) {
+
+    string titulo,cantante, genero;
+    int optionMenu=0, optionCola=0, duracion=0;
+    bool status = true;
+    node *newFeliz;
+
+        cout << endl << endl << endl;
+        cout << "\t\t\t\t\t\t\t**" << endl;
+        cout << "\t\t\t\t\t\t\t*****" << endl;
+        cout << "\t\t\t\t\t\t\t*******"<< endl;
+        cout << "\t\t\t\t\t\t\t**********" << endl;
+        cout << "\t\t\t\t\t\t\t*************              ____   __          _    __     __" << endl;
+        cout << "\t\t\t\t\t\t\t***************           |    | |  |        / *   *  *  /  /  " << endl;
+        cout << "\t\t\t\t\t\t\t******************        |  __| |  |       /   *   *  **  /" << endl;
+        cout << "\t\t\t\t\t\t\t*********************     | |    |  |___   /  *  *   *    /" << endl;
+        cout << "\t\t\t\t\t\t\t******************        |_|    |______| /_______*   |___|" << endl;
+        cout << "\t\t\t\t\t\t\t***************" <<endl;
+        cout << "\t\t\t\t\t\t\t*************" <<endl;
+        cout << "\t\t\t\t\t\t\t**********" <<endl;
+        cout << "\t\t\t\t\t\t\t*******" <<endl ;
+        cout << "\t\t\t\t\t\t\t*****" <<endl;
+        cout << "\t\t\t\t\t\t\t**" << endl;
+
+    while (status) {
     
+
+        cout << "\n" << "\t\t\t\t\t\t\t\t******* MENU DE USUARIO *******" << endl<<endl;
+        cout << "\t\t\t\t\t\t\t1. Ingresar cancion a cola de reproduccion" << endl;
+        cout << "\t\t\t\t\t\t\t2. Eliminar  cancion de la cola" << endl;
+        cout << "\t\t\t\t\t\t\t3. Vaciar cola de reproduccion" << endl;
+        cout << "\t\t\t\t\t\t\t4. Mostrar cola de reproduccion" << endl;
+        cout << "\t\t\t\t\t\t\t5. Agregar cancion a descargar" << endl;
+        cout << "\t\t\t\t\t\t\t6. Salir del menu" << endl;
+        cout<<"\t\t\t\t\t\t_________________________________________________________"<<endl;
+        cout << "\n" << "\t\t\t\t\t\t\tIngrese la opcion que deseas ejectuar: ";
+        cin >> optionMenu;
+        cin.ignore();
+        cout << "\n";
+
+        switch (optionMenu) {
+           
+            case 1:
+                cout<<"\t\t\t\t\t\t\tColas de reproduccion disponible: "<<endl;
+                cout<<"\t\t\t\t\t\t\t1. Electronica"<<endl;
+                cout<<"\t\t\t\t\t\t\t2. Regueton"<<endl;
+                cout<<"\t\t\t\t\t\t\t3. Rap"<<endl;
+                cout<<"\t\t\t\t\t\t\t4. Pop"<<endl;
+                cout<<"\t\t\t\t\t\t\t5. Salsa"<<endl;
+                cout<<"\t\t\t\t\t\t\t_____________________________________________________"<<endl;
+                cout<<"\t\t\t\t\t\t\tIngrese el numero de la cola a la que se le agregara: ";
+                cin>>optionCola;
+                cin.ignore();
+
+                    if (optionCola == 1){
+                        cout<<"Titulo: ";
+                        getline(cin,titulo);
+                        
+                        cout<<"Cantante: ";
+                        getline(cin,cantante);
+
+                        cout<<"Genero: ";
+                        getline(cin, genero );
+
+                        cout<<"Duracion: ";
+                        cin>>duracion;
+                        cin.ignore();
+
+                        push(electronica,titulo,cantante,genero,duracion);
+
+
+
+                    }else if (optionCola == 2){
+                        cout<<"Titulo: ";
+                        getline(cin,titulo);
+                        
+                        cout<<"Cantante: ";
+                        getline(cin,cantante);
+
+                        cout<<"Genero: ";
+                        getline(cin, genero );
+
+                        cout<<"Duracion: ";
+                        cin>>duracion;
+                        cin.ignore();
+
+                        push(regueton,titulo,cantante,genero,duracion);
+
+                    }else if (optionCola == 3){
+                        cout<<"Titulo: ";
+                        getline(cin,titulo);
+                        
+                        cout<<"Cantante: ";
+                        getline(cin,cantante);
+
+                        cout<<"Genero: ";
+                        getline(cin, genero );
+
+                        cout<<"Duracion: ";
+                        cin>>duracion;
+                        cin.ignore();
+
+                        push(rap,titulo,cantante,genero,duracion);
+
+                    }else if (optionCola == 4)
+                    {
+                        cout<<"Titulo: ";
+                        getline(cin,titulo);
+                        
+                        cout<<"Cantante: ";
+                        getline(cin,cantante);
+
+                        cout<<"Genero: ";
+                        getline(cin, genero );
+
+                        cout<<"Duracion: ";
+                        cin>>duracion;
+                        cin.ignore();
+
+                        push(Pop,titulo,cantante,genero,duracion);
+                    }else if (optionCola == 4)
+                    {
+                        cout<<"Titulo: ";
+                        getline(cin,titulo);
+                        
+                        cout<<"Cantante: ";
+                        getline(cin,cantante);
+
+                        cout<<"Genero: ";
+                        getline(cin, genero );
+
+                        cout<<"Duracion: ";
+                        cin>>duracion;
+                        cin.ignore();
+
+                        push(salsa,titulo,cantante,genero,duracion);
+                    }else
+                    {
+                        cout<<"Opcion incorrecta, intente de nuevo"<<endl;
+                    }
+              
+               
+                break;
+
+            case 2:
+                cout<<"\t\t\t\t\t\t\tColas de reproduccion disponible: "<<endl;
+                cout<<"\t\t\t\t\t\t\t1. Electronica"<<endl;
+                cout<<"\t\t\t\t\t\t\t2. Regueton"<<endl;
+                cout<<"\t\t\t\t\t\t\t3. Rap"<<endl;
+                cout<<"\t\t\t\t\t\t\t4. Pop"<<endl;
+                cout<<"\t\t\t\t\t\t\t5. Salsa"<<endl;
+                cout<<"\t\t\t\t\t\t\t_____________________________________________________"<<endl;
+                cout<<"\t\t\t\t\t\t\tIngrese el numero de la cola a la que se eliminara la cancion: ";
+                cin>>optionCola;
+                cin.ignore();
+
+                if(optionCola == 1){
+                    pop(electronica);
+                }else if(optionCola == 2){
+                    pop(regueton);
+                }else if(optionCola == 3){
+                    pop(rap);
+                }else if (optionCola == 4)
+                {
+                    pop(Pop);
+                }else if (optionCola == 5)
+                {
+                    pop(salsa);
+                }else
+                {
+                     cout<<"Opcion invalida, intente de nuevo"<<endl;
+                }
+                
+                break;
+
+            case 3:
+                cout<<"\t\t\t\t\t\t\tColas de reproduccion disponible: "<<endl;
+                cout<<"\t\t\t\t\t\t\t1. Electronica"<<endl;
+                cout<<"\t\t\t\t\t\t\t2. Regueton"<<endl;
+                cout<<"\t\t\t\t\t\t\t3. Rap"<<endl;
+                cout<<"\t\t\t\t\t\t\t4. Pop"<<endl;
+                cout<<"\t\t\t\t\t\t\t5. Salsa"<<endl;
+                cout<<"\t\t\t\t\t\t\t_____________________________________________________"<<endl;
+                cout<<"\t\t\t\t\t\t\tIngrese el numero de la cola que se vaciar: ";
+                cin>>optionCola;
+                cin.ignore();
+
+                if(optionCola == 1){
+                    eliminar(electronica);
+                }else if(optionCola == 2){
+                    eliminar(regueton);
+                }else if(optionCola == 3){
+                    eliminar(rap);
+                }else if (optionCola == 4)
+                {
+                    eliminar(Pop);
+                }else if (optionCola == 5)
+                {
+                    eliminar(salsa);
+                
+                }else{
+                    cout<<"Opcion invalida intente de nuevo"<<endl;
+                }
+                
+                break;
+            case 4:
+                cout<<"\t\t\t\t\t\t\tColas de reproduccion disponible: "<<endl;
+                cout<<"\t\t\t\t\t\t\t1. Electronica"<<endl;
+                cout<<"\t\t\t\t\t\t\t2. Regueton"<<endl;
+                cout<<"\t\t\t\t\t\t\t3. Rap"<<endl;
+                cout<<"\t\t\t\t\t\t\t4. Pop"<<endl;
+                cout<<"\t\t\t\t\t\t\t5. Salsa"<<endl;
+                cout<<"\t\t\t\t\t\t\t_____________________________________________________"<<endl;
+                cout<<"\t\t\t\t\t\t\tIngrese el numero de la cola que desea visualizar: ";
+                cin>>optionCola;
+                cin.ignore();
+
+                if(optionCola == 1){
+                    showQueue(electronica);
+                }else if(optionCola == 2){
+                    showQueue(regueton);
+                }else if(optionCola == 3){
+                    showQueue(rap);
+                }else if (optionCola == 4)
+                {
+                    showQueue(Pop);
+                }else if (optionCola == 5)
+                {
+                    showQueue(salsa);
+                
+                }else{
+                    cout<<"Opcion invalida intente de nuevo"<<endl;
+                }
+
+                break;
+
+            case 5:
+            cout << "\t\t\t\t\t\t\tIngrese los datos de la cancion a descargar: "<<endl;
+                cout<<"\t\t\t\t\t\t\tTitulo: ";
+                getline(cin,titulo);
+                        
+                cout<<"\t\t\t\t\t\t\tCantante: ";
+                getline(cin,cantante);
+
+                cout<<"\t\t\t\t\t\t\tGenero: ";
+                getline(cin, genero );
+
+                cout<<"\t\t\t\t\t\t\tDuracion: ";
+                cin>>duracion;
+                cin.ignore();
+
+                push(descargas,titulo,cantante,genero,duracion);
+                
+                break;
+
+            
+            case  6:
+                status = false;
+                break;
+
+
+            default:
+                cout<<"Opcion invalida, intente nuevamente."<<endl;
+               
+                break;
+        }
+    } 
+}
+
+void musica(queue * electrinica, queue *regueton, queue *rap, queue *pop, queue *salsa){
     
+    //llenado de cola de electronica
+    push(electrinica,"Wake me up", "Avicii", "Electronica", 277);
+    push(electrinica, "Animals", "Martin Garrix","Electronica", 191);
+
+
+    //llenado de cola regueton
+    push(regueton,"La Toxica", "Farruko","Regueton", 205);
+    push(regueton,"Bellaquita", "Dalex","Regueton",208);
+
+
+
+    //llenado de cola de rap
+    push(rap,"Till I Collapse","Eminem","Rap",298);
+    push(rap,"21 Questions ","50 Cent","Rap",258);
+
+
+    //llenado de cola de pop
+    push(pop,"Counting Stars","One Republic", "Pop",283);
+    push(pop, "Rude","MAGIC!", "Pop",225);
+
+    //llenado de cola de salsa
+    push(salsa,"Conteo Regresivo", "Gilberto Santa Rosa","Salsa",267);
+    push(salsa,"Valio la pena","Marc Anthony","Salsa",293);
 
 }
 
--- Spotify++.cpp
+++ Spotify++.cpp
@@ -9,21 +9,24 @@ void menu(queue *,queue *,queue *,queue *);
 
 int main(){
 
-    queue feliz, triste, estudiar, descargas;
+    queue electronica, regueton, rap, pop, salsa, descargas;
     registro sesion;
     bool terminos = false;
-    initialize(&feliz);
-    initialize(&triste);
-    initialize(&estudiar);
+    initialize(&electronica);
+    initialize(&rap);
+    initialize(&regueton);
+    initialize(&pop);
+    initialize(&salsa);
     initialize(&descargas);
 
     inicio(&sesion);
     
     terminosycondiciones(&terminos);
+    musica(&electronica,&regueton,&rap,&pop,&salsa);
 
     if (terminos == true)
     {
-        menu(&feliz,&triste,&estudiar,&descargas);
+        menu(&electronica,&regueton,&rap,&pop,&salsa,&descargas);
     }
     
 
@@ -32,248 +35,5 @@ int main(){
 
     getch();
     return 0;
-}
-
-void menu(queue *feliz, queue *triste, queue *estudiar, queue *descargas) {
-
-    string titulo,cantante, genero;
-    int optionMenu=0, optionCola=0, duracion=0;
-    bool status = true;
-    node *newFeliz;
-
-        cout << endl << endl << endl;
-        cout << "\t\t\t\t\t\t\t**" << endl;
-        cout << "\t\t\t\t\t\t\t*****" << endl;
-        cout << "\t\t\t\t\t\t\t*******"<< endl;
-        cout << "\t\t\t\t\t\t\t**********" << endl;
-        cout << "\t\t\t\t\t\t\t*************              ____   __          _    __     __" << endl;
-        cout << "\t\t\t\t\t\t\t***************           |    | |  |        / *   *  *  /  /  " << endl;
-        cout << "\t\t\t\t\t\t\t******************        |  __| |  |       /   *   *  **  /" << endl;
-        cout << "\t\t\t\t\t\t\t*********************     | |    |  |___   /  *  *   *    /" << endl;
-        cout << "\t\t\t\t\t\t\t******************        |_|    |______| /_______*   |___|" << endl;
-        cout << "\t\t\t\t\t\t\t***************" <<endl;
-        cout << "\t\t\t\t\t\t\t*************" <<endl;
-        cout << "\t\t\t\t\t\t\t**********" <<endl;
-        cout << "\t\t\t\t\t\t\t*******" <<endl ;
-        cout << "\t\t\t\t\t\t\t*****" <<endl;
-        cout << "\t\t\t\t\t\t\t**" << endl;
-
-    while (status) {
-        // cout << endl << endl << endl;
-        // cout << "\t\t\t\t\t\t\t**" << endl;
-        // cout << "\t\t\t\t\t\t\t*****" << endl;
-        // cout << "\t\t\t\t\t\t\t*******"<< endl;
-        // cout << "\t\t\t\t\t\t\t**********" << endl;
-        // cout << "\t\t\t\t\t\t\t*************              ____   __          _    __     __" << endl;
-        // cout << "\t\t\t\t\t\t\t***************           |    | |  |        / *   *  *  /  /  " << endl;
-        // cout << "\t\t\t\t\t\t\t******************        |  __| |  |       /   *   *  **  /" << endl;
-        // cout << "\t\t\t\t\t\t\t*********************     | |    |  |___   /  *  *   *    /" << endl;
-        // cout << "\t\t\t\t\t\t\t******************        |_|    |______| /_______*   |___|" << endl;
-        // cout << "\t\t\t\t\t\t\t***************" <<endl;
-        // cout << "\t\t\t\t\t\t\t*************" <<endl;
-        // cout << "\t\t\t\t\t\t\t**********" <<endl;
-        // cout << "\t\t\t\t\t\t\t*******" <<endl ;
-        // cout << "\t\t\t\t\t\t\t*****" <<endl;
-        // cout << "\t\t\t\t\t\t\t**" << endl;
-
-
-        cout << "\n" << "\t\t\t\t\t\t\t\t***** MENU DE COLAS *****" << endl<<endl;
-        cout << "\t\t\t\t\t\t\t1. Ingresar cancion a cola de reproduccion" << endl;
-        cout << "\t\t\t\t\t\t\t2. Eliminar primera cancion de la cola" << endl;
-        cout << "\t\t\t\t\t\t\t3. Vaciar cola de reproduccion" << endl;
-        cout << "\t\t\t\t\t\t\t4. Mostrar cola de reproduccion" << endl;
-        cout << "\t\t\t\t\t\t\t5. Agregar cancion a descargar" << endl;
-
-
-        cout << "\t\t\t\t\t\t\t6. Salir del menu" << endl;
-        cout<<"\t\t\t\t\t\t_________________________________________________________"<<endl;
-        cout << "\n" << "\t\t\t\t\t\t\tIngrese la opcion que deseas ejectuar: ";
-        cin >> optionMenu;
-        cin.ignore();
-        cout << "\n";
-
-        switch (optionMenu) {
-           
-            case 1:
-                cout<<"Colas de reproduccion disponible: "<<endl;
-                cout<<"1. Happy Life"<<endl;
-                cout<<"2. Sad Life"<<endl;
-                cout<<"3. Reay to Study"<<endl;
-                cout<<"_____________________________________________________"<<endl;
-                cout<<"Ingrese el numero de la cola a la que se le agregara: ";
-                cin>>optionCola;
-                cin.ignore();
-
-                    if (optionCola == 1){
-                        cout<<"Titulo: ";
-                        getline(cin,titulo);
-                        
-                        
-                        
-                        cout<<"Cantante: ";
-                        getline(cin,cantante);
-
-                        cout<<"Genero: ";
-                        getline(cin, genero );
-
-                        cout<<"Duracion: ";
-                        cin>>duracion;
-                        cin.ignore();
-
-                        push(feliz,titulo,cantante,genero,duracion);
-
-
-
-                    }else if (optionCola == 2){
-                        cout<<"Titulo: ";
-                        getline(cin,titulo);
-                        
-                        
-                        
-                        cout<<"Cantante: ";
-                        getline(cin,cantante);
-
-                        cout<<"Genero: ";
-                        getline(cin, genero );
-
-                        cout<<"Duracion: ";
-                        cin>>duracion;
-                        cin.ignore();
-
-                        push(triste,titulo,cantante,genero,duracion);
-
-                    }else if (optionCola == 3){
-                        cout<<"Titulo: ";
-                        getline(cin,titulo);
-                        
-                        
-                        
-                        cout<<"Cantante: ";
-                        getline(cin,cantante);
-
-                        cout<<"Genero: ";
-                        getline(cin, genero );
-
-                        cout<<"Duracion: ";
-                        cin>>duracion;
-                        cin.ignore();
-
-                        push(feliz,titulo,cantante,genero,duracion);
-
-                    }else 
-                        cout<<"Opcion incorrecta, intente de nuevo"<<endl;
-               
-                break;
-
-            case 2:
-                cout<<"Colas de reproduccion disponible: "<<endl;
-                cout<<"1. Happy Life"<<endl;
-                cout<<"2. Sad Life"<<endl;
-                cout<<"3. Reay to Study"<<endl;
-                cout<<"_____________________________________________________"<<endl;
-                cout<<"Ingrese el numero de la cola a la que se eliminara la cancion: ";
-                cin>>optionCola;
-                cin.ignore();
-
-                if(optionCola == 1){
-                    pop(feliz);
-                }else if(optionCola == 2){
-                    pop(triste);
-                }else if(optionCola == 3){
-                    pop(estudiar);
-                }else
-                {
-                    cout<<"Opcion invalida, intente de nuevo"<<endl;
-                }
-                
-                
-                break;
-            case 3:
-                cout<<"Colas de reproduccion disponible: "<<endl;
-                cout<<"1. Happy Life"<<endl;
-                cout<<"2. Sad Life"<<endl;
-                cout<<"3. Reay to Study"<<endl;
-                cout<<"_____________________________________________________"<<endl;
-                cout<<"Ingrese el numero de la cola que se vaciara: ";
-                cin>>optionCola;
-                cin.ignore();
-
-                if(optionCola == 1){
-                    eliminar(feliz);
-                }else if(optionCola==2){
-                    eliminar(triste);
-                }else if(optionCola==3){
-                    eliminar(estudiar);
-                }else{
-                    cout<<"Opcion invalida intente de nuevo"<<endl;
-                }
-                
-                break;
-            case 4:
-                cout<<"Colas de reproduccion disponible: "<<endl;
-                cout<<"1. Happy Life"<<endl;
-                cout<<"2. Sad Life"<<endl;
-                cout<<"3. Reay to Study"<<endl;
-                cout<<"_____________________________________________________"<<endl;
-                cout<<"Ingrese el numero de la cola que desea visualizar: ";
-                cin>>optionCola;
-                cin.ignore();
-
-                if(optionCola==1){
-                    showQueue(feliz);
-                    cout<<endl;
-
-                }else if(optionCola == 2){
-                    showQueue(triste);
-                    cout<<endl;
-
-                }else if(optionCola==3){
-                    showQueue(estudiar);
-                    cout<<endl;
-                }else{
-                    cout<<"Opcion invalida, intente de nuevo"<<endl;
-
-                }
-                break;
-            case 5:
-                cout<<"Titulo: ";
-                getline(cin,titulo);
-                        
-                cout<<"Cantante: ";
-                getline(cin,cantante);
-
-                cout<<"Genero: ";
-                getline(cin, genero );
-
-                cout<<"Duracion: ";
-                cin>>duracion;
-                cin.ignore();
-
-                push(descargas,titulo,cantante,genero,duracion);
-                
-                break;
-            
-            // case 6:
-            //     cout << "Ingrese el nombre de usurio: ";
-            //     getline(cin, sesion->nombre);
-            //     cout << "Correo electronico: ";
-            //     getline( cin, sesion->correo);
-            //     cout << "Contrasena: ";
-            //     getline( cin, sesion->contrasena);
-
-            //     cout << sesion->nombre <<" "<< sesion->correo<<endl; 
-
-            
-            case  6:
-                status = false;
-                break;
-
-
-            default:
-                cout<<"Opcion invalida, intente nuevamente."<<endl;
-               
-                break;
-        }
-    } 
 }
 
