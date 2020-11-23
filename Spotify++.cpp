e1dcd78440a521a8ed36e1435e92ba0f9539b1b2
Sirenoman <62576563+Sirenoman@users.noreply.github.com>
2020-11-23 01:54:24 -0600

    cambio para lista y que se registre nuevamente

--- Spotify++.cpp
+++ Spotify++.cpp
@@ -8,22 +8,29 @@ using namespace std;
 
 
 int main(){
-
     queue canciones;
     registro sesion;
-    bool terminos = false;
+    sesion.nombre = "Admin";
+    sesion.contrasena = "12345";
+    sesion.correo = "admin@gmail.com";
+    cuentas.push_back(sesion);
+
+    bool terminos = false, seguir=true;
     initialize(&canciones);
-
-    inicio(&sesion);
-    
-    terminosycondiciones(&terminos);
     musica(&canciones);
+    // La variable seguir es para que vuelva a registrarse en el programa si cierra su sesion
+    // pero si quiere iniciar sesion en otra cuenta pues esta la opcion 2
+    while(seguir){
+        inicio();
+        terminosycondiciones(&terminos);
 
-    if (terminos == true)
-    {
-        menu(&canciones);
+        if (terminos == true)
+        {
+            menu(&canciones, &seguir);
+        }
     }
     
+    system("cls");
     Sleep(100);
     return 0;
 }
